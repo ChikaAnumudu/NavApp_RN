d3674265d992e08e7aae2ed994b464f4eca23e1f
ChikaAnumudu <anumuduclintonchika@gmail.com>
2026-09-25 17:42:39 +0100

    Add a Modal Screen

--- components/ProductsScreen.jsx
+++ components/ProductsScreen.jsx
@@ -1,4 +1,4 @@
-import { StyleSheet, Button, View, Text, FlatList } from "react-native";
+import { StyleSheet, Button, View, Text, FlatList,Modal } from "react-native";
 import React, {useState} from 'react'
 import { BUTTONS } from '../models/ProductData'
 import ColorButton from "./ColorButton";
@@ -6,6 +6,7 @@ import ColorButton from "./ColorButton";
 
 const ProductsScreen = ({navigation}) => {
     const [buttons, setColoredButtons] = useState(BUTTONS);
+    const [isModalVisible, setModalVisible] = useState(false);
     const handleOnPress = () => {
         navigation.navigate("Home")
     };
@@ -25,6 +26,10 @@ const ProductsScreen = ({navigation}) => {
               }
             />
         );
+    }
+    const addNewColor = () => {
+      console.log('Add new color button pressed !!');
+      setModalVisible(true);
     }
   return (
     <View style={{ flex: 1, alignItems: "center" }}>
@@ -70,7 +75,23 @@ const ProductsScreen = ({navigation}) => {
         </View> */}
       </View>
 
-      <Button title="Go to Home page" onPress={handleOnPress} />
+      <Modal
+        visible={isModalVisible}
+        onRequestClose={() => setModalVisible(!isModalVisible)}
+        transparent={true}
+      >
+        <View style={{ height:'50%', width:'70%', justifyContent:'center', marginTop:'50%', alignSelf: 'center', alignItems: 'center', borderWidth: 2, borderRadius: 10, paddingHorizontal: 10, backgroundColor: '#f1e5d1'}}>
+          <Text style={{fontSize: 15, color: 'red', fontWeight: 'bold', }}>This is our modal window</Text>
+          <Button title="close" onPress={() => setModalVisible(!setModalVisible)}/>        
+        </View>
+
+      </Modal>
+      
+      <View style={{ flexDirection: "row", justifyContent: "space-around", width: "50%"}}>
+        <Button title="Go to Home page" onPress={handleOnPress} />
+        <Button title="Add" onPress={addNewColor} /> 
+      </View>
+
     </View>
   );
 }
